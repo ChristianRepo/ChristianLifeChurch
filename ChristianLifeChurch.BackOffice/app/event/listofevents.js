aa81d11114d05c8d67d3c33656a5343a5bfdb125
Eugen0907 <johnlexs@gmail.com>
2014-02-05 13:41:06 +0100

    Add some filles

---
+++ ChristianLifeChurch.BackOffice/app/event/listofevents.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'listofevents';
+
+    angular.module('app').controller(controllerId, ['common', 'repository', '$location', eventcontroller]);
+
+    function eventcontroller(common, repository, $location) {
+        var vm = this;
+        activate();
+
+        function activate() {
+            vm.title = 'Список событий';
+            vm.editEvent = editEvent;
+            vm.addNewEvent = addNewEvent;
+            vm.deleteEvent = deleteEvent;
+            var promises = [getEventsCount(), getAllEvents()];
+            common.activateController(promises, controllerId)
+                .then(function () {  });
+            
+        }
+
+        function addNewEvent() {
+            $location.path('/events/addeditevent/0');
+        }
+
+        function editEvent(event) {
+            $location.path('/events/addeditevent/' + event.id);
+        }
+
+        function deleteEvent(event) {
+            repository.deleteEvent(event.id).then(function (data) {
+                var logSuccess = common.logger.logSuccess;
+                logSuccess(data.message, data, controllerId, true);
+            }, function (data, status) {
+                var msg = data;
+                var logError = common.logger.logError;
+                logError(msg, data, controllerId, true);
+            });
+        }
+
+        function getEventsCount() {
+            repository.getAllEvents().then(function (data) {
+                console.log(data.length);
+            }, function (status) {
+                console.log(status);
+            });
+        }
+
+        function getAllEvents() {
+            repository.getAllEvents().then(function (data) {
+                vm.eventList = data;
+            }, function (status) {
+                console.log(status);
+            });
+        }
+   
+    }
+})();
